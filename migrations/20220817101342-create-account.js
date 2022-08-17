7625f482e135bc617d67d24c129d0bc315050d48
ImranNursandi <mrnnursandi@gmail.com>
2022-08-17 18:29:55 +0800

    commit

---
+++ migrations/20220817101342-create-account.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  up(queryInterface, Sequelize) {
+    return queryInterface.createTable('Accounts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      bankName: {
+        type: Sequelize.STRING
+      },
+      balance: {
+        type: Sequelize.INTEGER
+      },
+      accountNumber: {
+        type: Sequelize.STRING
+      },
+      type: {
+        type: Sequelize.STRING
+      },
+      UserId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        onDelete: 'cascade',
+        onUpdate: 'cascade'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Accounts');
+  }
+};
---
+++ models/profile.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Profile extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Profile.belongsTo(models.User)
+    }
+  }
+  Profile.init({
+    name: DataTypes.STRING,
+    address: DataTypes.STRING,
+    phoneNumber: DataTypes.STRING,
+    gender: DataTypes.STRING,
+    UserId: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Profile',
+  });
+  return Profile;
+};
